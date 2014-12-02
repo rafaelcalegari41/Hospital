36994a94aa483a851ae88795cb587d09f2c4f6ad
fernando <fernando@fernando-PC>
2014-12-02 21:06:04 -0200

    Padronização projeto

---
+++ Hospital/src/main/java/br/com/hospital/controller/LoginMB.java
@@ -0,0 +1,14 @@
+package br.com.hospital.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class LoginMB {
+	
+	public void login(){
+		
+	}
+
+}
