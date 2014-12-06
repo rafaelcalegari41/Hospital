3d649baf970e3e66491921c70c284cbe20225e64
Acer <Acer@Acer-PC>
2014-12-06 10:16:40 -0200

    Alterações nas telas listar e cadastrar plano de saúde.

---
+++ Hospital/src/main/java/br/com/hospital/controller/IndexMB.java
@@ -0,0 +1,15 @@
+
+package br.com.hospital.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class IndexMB {
+    
+    public void index(){
+		
+    }
+    
+}
