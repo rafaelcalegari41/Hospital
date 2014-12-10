9f502097209d6f9c0e65a397318fce91a83f9e79
fesmaniotto <fesmaniotto@AG146.expoente.com.br>
2014-12-10 17:21:43 -0200

    ..

---
+++ Hospital/src/main/java/br/com/hospital/business/MedicoBO.java
@@ -0,0 +1,30 @@
+package br.com.hospital.business;
+
+import java.util.List;
+
+import br.com.hospital.model.PessoaDAO;
+import br.com.hospital.pojo.Pessoa;
+
+public class MedicoBO {
+	
+	private PessoaDAO dao = new PessoaDAO();
+		
+	public boolean salvar(Pessoa pessoa){
+		System.out.println(pessoa.getPessNmNome());
+		
+		return dao.save(pessoa);
+	}
+	
+	public boolean atualizar(Pessoa pessoa){
+		return dao.update(pessoa);
+	}
+	
+	public boolean deletar(Pessoa pessoa){
+		return dao.delete(pessoa);
+	}
+	
+	public List<Pessoa> listar(){
+		return dao.findAll();
+	}
+
+}
