a56bc4b59ff212ae70230e2cf55d18009ffcb1d4
fernando <fernando@fernando-PC>
2015-01-25 15:03:14 -0200

    Rotinas de login

--- Hospital/src/main/java/br/com/hospital/controller/LoginMB.java
+++ Hospital/src/main/java/br/com/hospital/controller/LoginMB.java
@@ -1,14 +1,26 @@
 package br.com.hospital.controller;
 
+import java.io.IOException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 @ManagedBean
 @RequestScoped
 public class LoginMB {
 	
-	public void login(){
-		
+	public String login() throws ServletException, IOException{
+		ExternalContext contexto = FacesContext.getCurrentInstance().getExternalContext();
+		RequestDispatcher expedir = ((ServletRequest) contexto.getRequest()).getRequestDispatcher("/j_spring_security_check");
+		expedir.forward((ServletRequest) contexto.getRequest(), (ServletResponse) contexto.getResponse());
+		FacesContext.getCurrentInstance().responseComplete();		
+		return null;	
 	}
 
 }
